14cce359d762b2a17b4901ec60cd4427ff14e0ff
Rishabh Rastogi <rishabhrastogi047@gmail.com>
2023-04-30 13:51:07 +0530

    Create Rr.c

---
+++ Week_5/Rr.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+void findWaitingTime(int processes[], int n, int bt[], int wt[], int quantum)
+{
+	int rem_bt[n];
+    for (int i = 0 ; i < n ; i++)
+		rem_bt[i] = bt[i];
+    int t = 0;
+	while (1)
+    {
+		int done = 1;
+        for (int i = 0 ; i < n; i++)
+        {
+			if (rem_bt[i] > 0)
+			{
+				done = 0;
+				if (rem_bt[i] > quantum)
+                {
+					t += quantum;
+					rem_bt[i] -= quantum;
+                }
+				else
+                {
+					t = t + rem_bt[i];
+					wt[i] = t - bt[i];
+					rem_bt[i] = 0;
+                }
+            }
+        }
+		if (done == 1)
+			break;
+    }
+}
+
+void findTurnAroundTime(int processes[], int n, int bt[], int wt[], int tat[])
+{
+	for (int i = 0; i < n ; i++)
+		tat[i] = bt[i] + wt[i];
+}
+
+void findavgTime(int processes[], int n, int bt[], int quantum)
+{
+	int wt[n], tat[n], total_wt = 0, total_tat = 0;
+	
+	findWaitingTime(processes, n, bt, wt, quantum);
+	findTurnAroundTime(processes, n, bt, wt, tat);
+	
+	printf("PID\t\tBurst Time\tWaiting Time\tTurnaround Time");
+	for (int i=0; i<n; i++)
+    {
+		total_wt = total_wt + wt[i];
+        total_tat = total_tat + tat[i];
+        printf("\n%d\t\t%d\t\t%d\t\t%d", i+1, bt[i], wt[i], tat[i]);
+    }
+
+    float awt = (float)total_wt/n;
+    float avgtat = (float)total_tat/n;
+    printf("\nAverage Waiting Time = %.2f", awt);
+    printf("\nAverage Turnaround Time = %.2f", avgtat);
+    printf("\n");
+}
+
+int main()
+{
+	int n;
+	printf("Enter number of processes : ");
+    scanf("%d", &n);
+    int processes[5], bt[5];
+    for(int x=0; x<n; x++)
+    {
+		printf("Enter PID, BT\n");
+        scanf("%d%d", &processes[x], &bt[x]);
+    }
+    int q;
+    printf("Enter Time Quantum : ");
+    scanf("%d", &q);
+    findavgTime(processes, n, bt, q);
+    return 0;
+}
